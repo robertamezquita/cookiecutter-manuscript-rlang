1b5f6d24a2ba613e9eae44070071aaec3a0c0beb
Rob Amezquita <robert.amezquita@me.com>
2021-06-08 21:22:53 -0700

    bugfixes

--- {{cookiecutter.project_slug}}/_analysis/_make-site.sh
+++ {{cookiecutter.project_slug}}/_analysis/_make-site.sh
@@ -9,31 +9,16 @@ MODE=$1
 
 if [[ $MODE == "build" ]]; then
     # Make a _build directory where everything is flattened (all Rmds at same level)
-    mkdir -p _build/
-
-    # -- Manually amend this file to add any extra items to _build --
-    # Copy over supplemental folders that do not contain Rmd content but are needed for said Rmd content
-    cp -R _store-resources _build/
-    cp -R _notebooks _build/
-
-    # - top-level html/settings
-    cp _site.yml _build/
-    cp footer.html _build/
-
-    # Set the current directory to _build to use only the chosen transferred folder contents
-    # That way only Rmds that were in the notebooks folder are rendered for the site
-    cd build/
-
-    # Copy all Rmds, regardless of depth, place into top-level of _build/
-    RMDS=$(find . -name "*Rmd" | grep -v '_site' | grep -v '_build' | grep -v '_deprecated')
+    # Copy all Rmds, regardless of depth, place into top-level of repo for flat structure
+    RMDS=($(find -L . -name "*Rmd" | grep -v '_site' | grep -v '_build' | grep -v '_deprecated' | grep -v 'renv'))
     for RMD in ${RMDS[*]}; do
-        cp $RMD _build/
+        echo "Copying $RMD .."
+        cp $RMD .
     done
 fi
 
 if [[ $MODE == "render" ]]; then
     # Go inside _build to call the render step to prevent issues
-    cd _build/
-    R -e 'rmarkdown::render_site()' # _site.yml specifies output site as "../_site"
+    R -e 'renv::activate(); here::here(); rmarkdown::render_site()' # _site.yml specifies output site as "../_site"
     cd ..
 fi
--- {{cookiecutter.project_slug}}/_manuscript/_make-site.sh
+++ {{cookiecutter.project_slug}}/_manuscript/_make-site.sh
@@ -9,31 +9,16 @@ MODE=$1
 
 if [[ $MODE == "build" ]]; then
     # Make a _build directory where everything is flattened (all Rmds at same level)
-    mkdir -p _build/
-
-    # Copy over the manuscript add-ins
-    cp -R _figures _build/
-    cp -R _tables _build/
-    cp -R _resources _build/
-    
-    # - top-level html/settings
-    cp _site.yml _build/
-    cp _footer.html _build/
-
-    # Unlike _analysis, copy over all Rmds within the manuscript folder for rendering 
-    # Given that this area should only consist of prose, rendering/building extra files
-    # should be cheap; that said, placing anything within a _deprecated dir will ignore it
-
-    # Copy all Rmds, regardless of depth, place at top-level of _build
-    RMDS=$(find . -name "*Rmd" | grep -v '_site' | grep -v '_build' | grep -v '_deprecated' )
+    # Copy all Rmds, regardless of depth, place into top-level of repo for flat structure
+    RMDS=($(find -L . -name "*Rmd" | grep -v '_site' | grep -v '_build' | grep -v '_deprecated' | grep -v 'renv'))
     for RMD in ${RMDS[*]}; do
-        cp $RMD _build/
+        echo "Copying $RMD .."
+        cp $RMD .
     done
 fi
 
 if [[ $MODE == "render" ]]; then
     # Go inside _build to call the render step to prevent issues
-    cd _build/
-    R -e 'rmarkdown::render_site()' # _site.yml specifies output site as "../_site"
+    R -e 'renv::activate(); here::here(); rmarkdown::render_site()' # _site.yml specifies output site as "../_site"
     cd ..
 fi
